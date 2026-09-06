c64d1b01a3fb9839b968b0ce2f8d1af90ba58f31
WTL04 <williamluu04@gmail.com>
2026-09-06 14:59:01 -0700

    github pages config and mobile UI testing

--- app/src/components/Monogram.tsx
+++ app/src/components/Monogram.tsx
@@ -1,8 +1,10 @@
+import logo from '../assets/andy_vy_logo_white_transparent.png'
+
 export default function Monogram() {
   return (
     <img
       className="monogram"
-      src="/imgs/andy_vy_logo_white_transparent.png"
+      src={logo}
       alt="A & V monogram"
     />
   )
